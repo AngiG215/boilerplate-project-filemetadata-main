cdb213faf5b970ae25c3fb7fd737ed7f746d4371
Angela <angieadrigarcia572@gmail.com>
2026-02-25 23:10:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.get('/', function (req, res) {
   }
 
   res.json({
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
   });
 });
 var port = process.env.PORT || 3000;
